fc2ad615523d9f4e24c41b86711d50fa0215a43b
Shaylyne <43526282+RonniesaPayne@users.noreply.github.com>
2019-04-17 18:47:44 -0500

    added ajax base code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
 
 
 })
