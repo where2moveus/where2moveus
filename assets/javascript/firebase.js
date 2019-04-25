3be5579e2ca6ac96b220a4fd85dfaa5c230da77e
Roy Park <donghuip90@gmail.com>
2019-04-24 20:54:10 -0500

    code is somewhat long but the login and signup functions all work

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -10,15 +10,29 @@ var config = {
 
   firebase.initializeApp(config);
 
-//On click of "Submit", email and pass are used to signup
-  document.getElementById("btnSubmit").addEventListener('click', e=>{
-    const email = document.getElementById("signUpEmail").value;
-    const pass = document.getElementById("signUpPW").value;
-    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-     console.log(error.message);
-    });
-  })
-
+//REGISTER THE USER
+  $(`#btnSubmit`).click(function () {
+    event.preventDefault();
+  
+    var email = $(`#signUpEmail`).val().trim();
+    var password = $(`#signUpPW`).val().trim();
+  
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function (action) {
+        console.log("Thank you for signing up")
+  
+  //Successful REGISTER redirects to index.html
+        window.location.href = 'index.html';
+  
+    }).catch(function (error) {
+       // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(error);
+  return error;
+  // ...
+});
+  
+  });
 
 //This chunk of code stores the email and password to the database
 
@@ -47,3 +61,38 @@ var config = {
     });
 
 });
+
+//This line of code handles the Log-In
+//On click of login button
+$(`#btnLogin`).click(function () {
+  event.preventDefault();
+
+  var email = $(`#txtEmail`).val().trim();
+  var password = $(`#txtPassWord`).val().trim();
+
+//Check within firebase if its registered user(?)
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function (action) {
+      console.log("You are Logged In Redirecting to the Homepage")
+
+//Successful log in redirects to cities.html
+      window.location.href = 'cities.html';
+
+  }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(error);
+      return error;
+
+  });
+
+})
+
+// //On click of "Submit", email and pass are used to signup
+// document.getElementById("btnSubmit").onClick('click', e=>{
+//   const email = document.getElementById("signUpEmail").value;
+//   const pass = document.getElementById("signUpPW").value;
+//   firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+//    console.log(error.message);
+//   });
+// })  
