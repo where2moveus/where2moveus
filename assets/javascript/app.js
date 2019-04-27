6f05675b8e276b3b60c85577f203db47d6e37161
Shaylyne <43526282+RonniesaPayne@users.noreply.github.com>
2019-04-27 09:51:32 -0500

    do NOT merge this change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,47 @@
 $(document).ready(function() {
-    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyAvvwmXICbkoU9Y3Bq5BLyeMGaaqnNnYAI";
+  
+  var citiesArr = [];
 
-    $.ajax({
+
+  //function displayPhotos() {
+  var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=cities&key=AIzaSyAvvwmXICbkoU9Y3Bq5BLyeMGaaqnNnYAI"
+  
+      $.ajax({
       url: queryURL,
       method: "GET",
-      dataType: 'jsonp',
-            cache: false,
-            success: function(response){                          
-                alert(response); 
-            } 
-    }).then(function(response) {
+        //dataType: "jsonp",
+            //cache: false,
+            //success: function(response){                          
+                //alert(response); 
+            //}
+            }).then(function(response) {
+      $("#cityCards").text(JSON.stringify(response));
+      console.log(queryURL);
       console.log(response);
-    });
+    })
+      //var photos = response.largeImageURL;
 
+  //putting place in card div
+     $("#csSubmit").on("click", function(event) {
+       event.preventDefault();
+      var city = $("#searchFor").val().trim();
+
+
+      
+        //var photos = response.largeImageURL;
+        //city.append(photos);
+        //$("#csSubmit").append(photos)
+
+        // Adding city from the textbox to our array
+        //citiesArr.push(city);
+      //})
+        //$(document).on("click", displayPhotos);
+
+
+
+        //https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJdd4hrwug2EcRmSrV3Vo6llI&key=API_KEY
+
+    })
 
 })
+
